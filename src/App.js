c7bec5662a24c95d376139a9f051fd53d7a6245b
gokul karki <gokulkarki987654@gmail.com>
2023-08-19 13:33:42 +0530

    intial commit for react with docker

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      gokul123_4 coolboy
+      <h1> yaya its done betcmonter</h1>
+    </div>
+  );
+}
+
+export default App;
