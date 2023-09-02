0c837fb7e72e08bda40dbb0d56b2ead49861c253
gokul karki <gokulkarki987654@gmail.com>
2023-09-02 15:55:58 +0530

    changed some files to fix the test

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      gokul123_4 coolboy
-      <h1> yaya its done betcmonter</h1>
+       Hello World
     </div>
   );
 }
